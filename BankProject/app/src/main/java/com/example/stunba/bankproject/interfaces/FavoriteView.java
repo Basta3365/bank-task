240d53a3b908035adbf6104485316426d1195d72
Kseniya Bastun <Kseniya_Bastun@epam.com>
2017-09-26 15:47:52 +0300

    demo 4

--- BankProject/app/src/main/java/com/example/stunba/bankproject/interfaces/FavoriteView.java
+++
@@ -1,9 +0,0 @@
-package com.example.stunba.bankproject.interfaces;
-
-/**
- * Created by Kseniya_Bastun on 9/8/2017.
- */
-
-public interface FavoriteView {
-        void showError(String str);
-}
--- BankProject/app/src/main/java/com/example/stunba/bankproject/presenters/ipresenters/BaseInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.stunba.bankproject.presenters.ipresenters;
-
-/**
- * Created by Kseniya_Bastun on 9/15/2017.
- */
-
-public interface BaseInterface {
-}
